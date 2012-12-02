7ddd5a9fee64444b8782ca8617d1cca9953e54e6
Nicolas Canceill <nicolas@570ffimac.home>
2012-12-02 17:38:28 +0100

    Algorithm complete. TODO: create output pdf; check chaokey method; select mu)

--- pdf_stego.py
+++ pdf_stego.py
@@ -12,7 +12,6 @@ import hashlib
 
 # Handle PDF files
 class PDF_file:
-	
 	file_name = ''
 	
 	def __init__(self,file_name):
@@ -22,6 +21,7 @@ class PDF_file:
 		os.system('qpdf '+self.file_name+' '+self.file_name+'.uncomp.pdf --stream-data=uncompress')
 	
 	def compress(self):
+		# TODO
 		return 0
 
 # Handle 015 numeral integers
@@ -62,35 +62,91 @@ class Numerals:
 
 # Generate chaotic maps
 class Chaotic:
-	passkey = ''
-	mu = 0
+	mu = 3.6
+	x = 0.5
 	
-	def __init__(self,passkey,mu):
-		self.passkey = passkey
+	def __init__(self,mu,flagstr):
 		self.mu = mu
+		self.x = self.gen_chaokey(flagstr)
 	
-	def next(self,x):
-		return self.mu * x * (1 - x)
+	def gen_chaokey(self,flagstr):
+		dec = ""
+		for i in flagstr:
+			dec = dec + str(i)
+		return float("0." + dec)
+	
+	def next(self):
+		x_ = self.mu * self.x * (1 - self.x)
+		self.x = x_
+		return self.x
 
 # Perform the stego algorithm
 class PDF_stego:
+	redundancy = 0.1
+	mu_one = 3.7
+	mu_two = 3.8
 	passkey = ''
+	data = ''
 	
-	def __init__(self,name,passkey):
+	def __init__(self,name,passkey,data):
 		self.passkey = passkey
+		self.data = data
 		self.file_op = PDF_file(name)
 		self.file_op.uncompress()
 	
+	# Returns a list res[]
+	# If res[0] == True then num was embedded, move on to next numeral
+	# If res[0] == False then try to embed num again in the nex operator
+	# res[1] is the new operator value (regardless of res[0])
+	# Pass None as num to significate that the IND has been taken to finish
+	def embed_op(self,val,ch_one,ch_two,num):
+		if abs(val) > 16:
+			return [False,val]
+		if ch_two < self.redundancy or num == None:
+			return [False,ch_one]
+		if val < 0:
+			return [True, -num]
+		return [True, num]
+
+	def embed_line(self,line,ch_one,ch_two,ind,i):
+		newline = line
+		i_ = i
+		k = 0
+		while k < newline.__len__():
+			# WARNING: use "re.search", DO NOT use "re.match"
+			m = re.search(r'\)(\-?[0-9]+)\(',newline[k:])
+			if m == None:
+				k = newline.__len__()
+			else:
+				tj = int(m.group(1))
+				if i_ < ind.__len__():
+					op = self.embed_op(tj,ch_one.next(),ch_two.next(),ind[i_])
+				else:
+					op = self.embed_op(tj,ch_one.next(),ch_two.next(),None)
+				if op[0]:
+					i_ += 1
+				newline = newline[:k + m.start(1)] + str(op[1]) + newline[k + m.end(1):]
+				k = k + m.start(1) + str(op[1]).__len__()
+		return [newline,i_]
+	
 	def embed(self):
 		cover_file = open(self.file_op.file_name + ".uncomp.pdf")
+		new_file = ""
+		nums = Numerals().encode(self.data, self.passkey)
+		ch_one = Chaotic(self.mu_one,nums[2])
+		ch_two = Chaotic(self.mu_two,nums[2])
+		ind = nums[0] + nums[1] + nums[2]
+		i = 0
 		for line in cover_file:
-			m = re.search(r'\[.*\][ ]?TJ',line) #WARNING: use "search", DO NOT use "match"
-			if m != None:
-				print(line+ "\n") 
-			
-	
-	def sub_embed(self,line):
-		return 0
+			# WARNING: use "re.search", DO NOT use "re.match"
+			m = re.search(r'\[(.*)\][ ]?TJ',line)
+			if m == None:
+				new_file += line
+			else:
+				newline = self.embed_line(m.group(1),ch_one,ch_two,ind,i)
+				new_file += line[:m.start(1)] + newline[0] + line[m.end(1):]
+				i = newline[1]
+		return new_file # TODO create and compress new pdf
 
 #
 #
@@ -107,11 +163,13 @@ def print_nums(name, nums):
 #
 # TESTS & EXAMPLES
 #
-		
-#ps = PDF_stego("test.pdf","abcd")
-#ps.embed()
 
+# Encoding message and key to numerals
 #nums = Numerals().encode("lorem ipsum sin dolor amet", "abcd1234")
 #print_nums('Flagstr1', nums[0])
 #print_nums('Message', nums[1])
 #print_nums('Flagstr2', nums[2])
+
+# Running the whole alogorithm
+#ps = PDF_stego("test.pdf","lorem ipsum sin dolor amet","abcd1234")
+#ps.embed()
