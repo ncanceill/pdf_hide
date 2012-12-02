52d3633db80aa0b15d60751afcb169063282a525
ncanceill <ncanceill@budapest.studlab.os3.nl>
2012-12-02 12:39:34 +0100

    Added 015 numeral integers manipulation. Cosmetic and comments.

--- pdf_stego.py
+++ pdf_stego.py
@@ -1,19 +1,82 @@
 #!/usr/bin/python
 import os
 import re
+import binascii
+import hashlib
 
+#
+#
+#
+# CLASSES
+#
+
+# Handle PDF files
 class PDF_file:
+	
 	file_name = ''
-	def __init__(self, file_name):
+	
+	def __init__(self,file_name):
 		self.file_name = file_name
+	
 	def uncompress(self):
 		os.system('qpdf '+self.file_name+' '+self.file_name+'.uncomp.pdf --stream-data=uncompress')
+	
 	def compress(self):
-		return 0;
+		return 0
+
+# Handle 015 numeral integers
+class Numerals:
+	#nbits = 4
+	
+	def str_to_binstr(self,str):
+		return bin(int(binascii.hexlify(str), 16))
+
+	def binstr_to_num(self,str):
+		return int(str,2) % 16
+	
+	#def bin_to_binstr(self,bin):
+	#	return str(bin) if bin<=1 else bin(bin>>1) + str(bin&1)
+	
+	#def bin_to_str(self,bin):
+	#	return binascii.unhexlify('%x' % int(bin,2))
+	
+	def hexstr_to_num(self,h):
+		return int(h,16) % 16
+	
+	def split_len(self,seq,length):
+		return [seq[i:i+length] for i in range(0,len(seq),length)]
+	
+	def digest(self,str):
+		h = hashlib.sha1()
+		h.update(str)
+		return h.hexdigest()
+	
+	def digest_to_nums(self,d):
+		return map(self.hexstr_to_num,self.split_len(d,2))
+	
+	def msg_to_nums(self,msg):
+		return map(self.binstr_to_num,self.split_len(self.str_to_binstr(msg),4))
+	
+	def encode(self,msg,key):
+		return [self.digest_to_nums(self.digest(msg)),self.msg_to_nums(msg),self.digest_to_nums(self.digest(key))]
+
+# Generate chaotic maps
+class Chaotic:
+	passkey = ''
+	mu = 0
+	
+	def __init__(self,passkey,mu):
+		self.passkey = passkey
+		self.mu = mu
+	
+	def next(self,x):
+		return self.mu * x * (1 - x)
+
+# Perform the stego algorithm
 class PDF_stego:
 	passkey = ''
 	
-	def __init__(self, name, passkey):
+	def __init__(self,name,passkey):
 		self.passkey = passkey
 		self.file_op = PDF_file(name)
 		self.file_op.uncompress()
@@ -27,13 +90,28 @@ class PDF_stego:
 			
 	
 	def sub_embed(self,line):
-		return 0;
+		return 0
+
+#
+#
+#
+# UTILITIES
+#
+
+def print_nums(name, nums):
+	print '===== ' + name + ' ====='
+	print nums
+
+#
+#
+#
+# TESTS & EXAMPLES
+#
 		
-class Chaotic:
-	passkey = ''
-	
-	def __init__(self,passkey):
-		self.passkey = passkey
-		
-ps = PDF_stego("test.pdf","abcd") 	
-ps.embed()		
+#ps = PDF_stego("test.pdf","abcd")
+#ps.embed()
+
+#nums = Numerals().encode("lorem ipsum sin dolor amet", "abcd1234")
+#print_nums('Flagstr1', nums[0])
+#print_nums('Message', nums[1])
+#print_nums('Flagstr2', nums[2])
