1b855a3abcb6d8b02e2bcb66ca8c8b814217dc20
Nicolas Canceill <nicolas@570ffimac.home>
2012-12-06 20:06:17 +0100

    Cosmetic & comments.

--- pdf_stego.py
+++ pdf_stego.py
@@ -16,19 +16,23 @@ class PDF_file:
 	def __init__(self,file_name):
 		self.file_name = file_name
 
+	# Generates QDF file "<file>.qdf" from PDF file "<file>", uncompressing streams if needed
 	def uncompress(self):
 		os.system('qpdf '+self.file_name+' '+self.file_name+'.qdf --qdf --stream-data=uncompress')
 
+	# Generates fixed QDF  file "<file>.fix" from damaged QDF file "<file>", reconstructing XRef and trailer if needed
 	def fix(self):
 		os.system('fix-qdf <'+self.file_name+' >'+self.file_name+'.fix')
-	
+
+	# Generates PDF file "<file>.pdf" from QDF or PDF file "<file>", compressing streams if needed
 	def compress(self):
 		os.system('qpdf '+self.file_name+' '+self.file_name+'.pdf --stream-data=compress')
 
 # Handle 015 and 116 numeral integers, and binary strings, and other stuff
 class Numerals:
 	#nbits = 4
-	
+
+	# Encodes a string into a binary string based on the ASCII codes (e.g. "a" returns "01100001")
 	def str_to_binstr(self,str):
 		if str.__len__() < 1:
 			return ""
@@ -38,48 +42,66 @@ class Numerals:
 				b = "0" + b
 			return b + self.str_to_binstr(str[1:])
 
+	# Encodes a 4-bit number (passed-in as a binary string, e.g. "0110") into a "015" numeral
 	def binstr_to_num(self,str):
 		return int(str,2) % 16
-	
+
+	# Encodes an ASCII code (passed-in as an hexadecimal string) into a "015" numeral using mod(16)
 	def hexstr_to_num(self,h):
 		return int(h,16) % 16
 
+	# Decodes a character from two "015" numerals according to the algo
 	def nums_to_ch(self,i,j):
 		return chr(16 * i + j)
-	
+
+	# Splits a sequence into a list of sequences of specified length (the last one may be shorter)
 	def split_len(self,seq,length):
 		return [seq[i:i+length] for i in range(0,len(seq),length)]
-	
+
+	# Returns the 20-byte SHA1 digest of a string as an hexadecimal string
 	def digest(self,str):
 		return hashlib.sha1(str).hexdigest()
-	
+
+	# Encodes a 20-byte SHA1 digest to a list of 20 "015" numerals array according to the algo
 	def digest_to_nums(self,d):
 		return map(self.hexstr_to_num,self.split_len(self.digest(d),2))
-	
+
+	# Encodes a message to a list of "015" numerals according to the algo
 	def msg_to_nums(self,msg):
 		return map(self.binstr_to_num,self.split_len(self.str_to_binstr(msg),4))
-	
+
+	# Encodes a message and a stego key according to the algo
+	#
+	# Returns a list n[]:
+	# n[0] is the list of 20 "015" numerals representing "FlagStr1"
+	# n[1] is the list of "015" numerals representing the message
+	# n[2] is the list of 20 "015" numerals representing "FlagStr2"
 	def encode_msg(self,msg,key):
 		return [self.digest_to_nums(msg),self.msg_to_nums(msg),self.digest_to_nums(key)]
-	
+
+	# Encodes a derived key according to the algo
+	#
+	# Returns the list of 20 "015" numerals representing "FlagStr"
 	def encode_key(self,key):
 		return self.digest_to_nums(key)
 
 # Generate chaotic maps
 class Chaotic:
 	mu = 3.6
-	x = 0.5
+	x = 0
 	
 	def __init__(self,mu,flagstr):
 		self.mu = mu
 		self.x = self.gen_chaokey(flagstr)
-	
+
+	# Generates a chaotic key (seed for the chaotic map) according to the algo
 	def gen_chaokey(self,flagstr):
 		dec = ""
 		for i in flagstr:
 			dec = dec + str(i)
 		return float("0." + dec)
-	
+
+	# Gets the next real number from the chaotic map
 	def next(self):
 		x_ = self.mu * self.x * (1 - self.x)
 		self.x = x_
@@ -87,51 +109,88 @@ class Chaotic:
 
 # Perform the stego algorithm
 class PDF_stego:
+	# Debug logging flag
 	debug = False
+
+	# Redundancy parameter, should be in ]0,1[
 	redundancy = 0.1
+
+	# Chaotic map parameters, should be in ]3.57,4[
 	mu_one = 3.7
-	mu_two = 3.9
+	mu_two = 3.8
+
+	# Counter for TJ operators
 	tj_count = 0
 	
 	def __init__(self,input,debug):
 		self.file_op = PDF_file(input)
 		self.debug = debug
-	
+
+	# Embeds data in a TJ operator
+	#
+	# val: the original value of the TJ operator
+	# ch_one: the next number from chaotic map 1
+	# ch_two: the next number from chaotic map 2
+	# num: the next numeral to embed, or None if the IND was taken to finish
+	#
 	# Returns a list res[]
 	# If res[0] == True then num was embedded, move on to next numeral
 	# If res[0] == False then try to embed num again in the nex operator
 	# res[1] is the new operator value (regardless of res[0])
-	# Pass None as num to significate that the IND has been taken to finish
 	def embed_op(self,val,ch_one,ch_two,num):
 		self.tj_count += 1
 		if abs(val) > 16 or val == 0:
+			# Do not use TJ op
 			return [False,val]
 		if ch_two < self.redundancy or num == None:
+			# Use TJ op for a random value
 			return [False,int(15*ch_one)+1]
+		# Use TJ op for data
 		if val < 0:
 			return [True, -num - 1]
 		return [True, num + 1]
 
+	# Embeds data in TJ operators from a TJ string
+	#
+	# line: the TJ string to parse
+	# ch_one: chaotic map 1
+	# ch_two: chaotic map 2
+	# ind: the IND to embed
+	# i: the current IND index
+	#
+	# Returns a list res[]
+	# res[0] is the modified line
+	# res[1] is the new value of the IND index
 	def embed_line(self,line,ch_one,ch_two,ind,i):
 		newline = line
 		i_ = i
 		k = 0
 		while k < newline.__len__():
+			# Parse TJ string from current position
 			m = re.search(r'[>)](\-?[0-9]+)[<(]',newline[k:])
 			if m == None:
+				# No more TJ ops
 				k = newline.__len__()
 			else:
 				tj = int(m.group(1))
 				if i_ < ind.__len__():
+					# Try to embed numeral
 					op = self.embed_op(tj,ch_one.next(),ch_two.next(),ind[i_])
 				else:
+					# No more numerals to embed
 					op = self.embed_op(tj,ch_one.next(),ch_two.next(),None)
 				if op[0]:
+					# One numeral was embedded, update IND index
 					i_ += 1
+				# Insert new value
 				newline = newline[:k + m.start(1)] + str(op[1]) + newline[k + m.end(1):]
+				# Update current position
 				k += m.start(1) + str(op[1]).__len__()
 		return [newline,i_]
-	
+
+	# Embeds data with passkey in a PDF file "<file>", outputs stego PDF file "<file>.out.fix.pdf"
+	#
+	# Returns the number of embedded "015" numbers constituting the data
 	def embed(self,data,passkey):
 		self.tj_count = 0
 		if self.debug:
@@ -141,27 +200,36 @@ class PDF_stego:
 		cover_file = open(self.file_op.file_name + ".qdf")
 		new_file = ""
 		n = Numerals()
+		# Get the numerals to embed from the key and the message
 		nums = n.encode_msg(data,passkey)
 		if self.debug:
 			print_nums('FlagStr1 (CheckStr)',nums[0])
 			print_nums('FlagStr2',nums[2])
 			print_nums('Data',n.msg_to_nums(data))
+		# Initiate chaotic maps
 		ch_one = Chaotic(self.mu_one,nums[2])
 		ch_two = Chaotic(self.mu_two,nums[2])
 		ind = nums[0] + nums[1] + nums[2]
 		i = 0
+		# Parse file
 		for line in cover_file:
+			# Parse line for TJ blocks
 			m = re.search(r'\[(.*)\][ ]?TJ',line)
 			if m == None:
+				# No TJ blocks
 				new_file += line
 			else:
+				# Try to embed data in TJ block
 				newline = self.embed_line(m.group(1),ch_one,ch_two,ind,i)
+				# Insert new block
 				new_file += line[:m.start(1)] + newline[0] + line[m.end(1):]
 				i = newline[1]
 		cover_file.close()
 		print "Embedded:\n\"" + data + "\""
 		if i < ind.__len__():
 			print "Error: not enough space available"
+			if self.debug:
+				print "\n========== END EMBED ==========\n"
 			return 0
 		else:
 			output_file = open(self.file_op.file_name + ".out","w")
@@ -172,30 +240,38 @@ class PDF_stego:
 			output_fixed = PDF_file(self.file_op.file_name + ".out.fix")
 			output_fixed.compress()
 			print "Wrote compressed PDF to \"" + self.file_op.file_name + ".out.fix.pdf\" with " + str(self.tj_count) + " TJ ops (" + str(nums[1].__len__()) + " of them used for data)\n"
+			if self.debug:
+				print "\n========== END EMBED ==========\n"
 			return nums[1].__len__()
-		if self.debug:
-			print "\n========== END EMBED ==========\n"
 
 	def extract_op(self,val,ch_two):
 		self.tj_count += 1
 		if abs(val) > 16 or val == 0 or ch_two < self.redundancy:
+			# Do not use TJ op
 			return 0
+		# Extract data from TJ op
 		return abs(val)
 
 	def extract_line(self,line,ch_two):
 		k = 0
 		tjs = []
 		while k < line.__len__():
+			# Parse TJ string from current position
 			m = re.search(r'[>)](\-?[0-9]+)[<(]',line[k:])
 			if m == None:
+				# No more TJ ops
 				k = line.__len__()
 			else:
+				# Try to extract numeral
 				tj = self.extract_op(int(m.group(1)),ch_two.next())
 				if tj != 0:
+					# Get value
 					tjs += [tj - 1]
+				# Update current position
 				k += m.end(1)
 		return tjs
 
+	# Extracts data of known length from PDF file "<file>" using derived_key, outputs extracted data to "<file>.embd"
 	def extract(self,derived_key,length):
 		self.tj_count = 0
 		if self.debug:
@@ -205,14 +281,19 @@ class PDF_stego:
 		self.file_op.uncompress()
 		embedding_file = open(self.file_op.file_name + '.qdf')
 		n = Numerals()
+		# Get the numerals from the key
 		nums = n.encode_key(derived_key)
 		if self.debug:
 			print_nums('FlagStr',nums)
+		# Initiate chaotic map
 		ch_two = Chaotic(self.mu_two,nums)
 		tjs = []
+		# Parse file
 		for line in embedding_file:
+			# Parse line for TJ blocks
 			m = re.search(r'\[(.*)\][ ]?TJ',line)
 			if m != None:
+				# Try to extract data from TJ block
 				tjs += self.extract_line(line,ch_two)
 		embedding_file.close()
 		if tjs.__len__() < 40 + length:
@@ -222,6 +303,7 @@ class PDF_stego:
 			embedded = tjs[20:tjs.__len__() - 20]
 			k = 0
 			emb_str = ""
+			# Decode embedded data
 			while k < length - 1:
 				emb_str += n.nums_to_ch(embedded[k],embedded[k + 1])
 				k += 2
@@ -229,6 +311,7 @@ class PDF_stego:
 				print_nums('Data Checksum',n.encode_key(emb_str))
 				print_nums('CheckStr',checkstr)
 				print_nums('Data',n.msg_to_nums(emb_str))
+			# Check integrity
 			if n.digest_to_nums(emb_str) != checkstr:
 				print "Error: CheckStr does not match embedded data from " + str(self.tj_count) + " TJ ops (" + str(tjs.__len__() - 40) + " of them used for data)"
 				if self.debug:
