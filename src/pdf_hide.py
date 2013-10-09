c5cedf462fa05c9a69469ddcedf797fe20f2c8c9
ncanceill <nicolas.canceill@ens-cachan.org>
2013-10-09 22:04:44 +0200

    improved program help

--- src/pdf_hide.py
+++ src/pdf_hide.py
@@ -50,32 +50,41 @@ def main():
 	# CLI
 	parser = argparse.ArgumentParser(prog="pdf_hide", formatter_class=argparse.RawDescriptionHelpFormatter,
 						description=logger.MSG_DESC, epilog=logger.MSG_LICENSE)
-	subparsers = parser.add_subparsers(title="Actions", dest="action",
+	subparsers = parser.add_subparsers(title="actions", dest="action",
 									   help="action to execute")
-	parser_embed = subparsers.add_parser("embed", help="Embed message inside PDF file")
-	parser_extract = subparsers.add_parser("extract", help="Extract message from PDF file")
+	# CLI - General
 	parser.add_argument("filename",
 						help="PDF file (may be compressed) to use as input")
 	parser.add_argument("-k", "--key", dest="key",
 						help="use KEY as the stego-key", metavar="KEY")
+	# CLI - Embedding
+	parser_embed = subparsers.add_parser("embed", help="Embed message inside PDF file")
 	parser_embed.add_argument("-m", "--message", dest="msg",
 						help="use MESSAGE as the data to embed", metavar="MESSAGE")
-	parser.add_argument("-n", "--nbits", dest="nbits", action="store", type=int, default=4,
+	parser_embed.add_argument("--no-random", action="store_true", dest="norandom", default=False,
+						help="do not embed random values, keep original ones")
+	# CLI - Extracting
+	parser_extract = subparsers.add_parser("extract", help="Extract message from PDF file")
+	# CLI - Options
+	group_options = parser.add_argument_group("algorithm options", "use these options to tune the algorithm")
+	group_options.add_argument("-n", "--nbits", dest="nbits", action="store", type=int, default=4,
 						help="use NBITS as the number of bits to use for numerals", metavar="NBITS")
-	parser.add_argument("-r", "--redundancy", dest="red", action="store", type=float, default=0.1,
+	group_options.add_argument("-r", "--redundancy", dest="red", action="store", type=float, default=0.1,
 						help="use RED as the redundancy parameter (strictly between 0 and 1)", metavar="RED")
+	# CLI - Improvements
 	parser.add_argument("-i", "--improve", action="store_true", dest="improve", default=False,
 						help="use algo improvements")
-	parser.add_argument("--no-random", action="store_true", dest="norandom", default=False,
-						help="do not embed random values, keep original ones (ignored if extracting)")
 	parser.add_argument("--custom-range", action="store_true", dest="customrange", default=False,
 						help="use data in [-450,-250] without -333 and -334 (ignored with original algo, should always be used in combination with --no-random when embedding)")
+	# CLI - Verbosity
 	group_verb = parser.add_mutually_exclusive_group()
 	group_verb.add_argument("-v", "--verbose", action="count", dest="verbose", default=0,
 						help="set verbosity level")
 	group_verb.add_argument("-q", "--quiet", action="store_const", dest="verbose", const=-1,
 						help="force quiet output")
-	parser.add_argument("--version", action="version", version=logger.MSG_VERSION)
+	# CLI - Version
+	group_version = parser.add_argument_group("version")
+	group_version.add_argument("--version", action="version", version=logger.MSG_VERSION)
 	args = parser.parse_args()
 	# Log
 	rl = logger.rootLogger(args.verbose)
