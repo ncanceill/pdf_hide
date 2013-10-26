38b62834ddecd9f4da256da4a0750568b6047d3a
ncanceill <nicolas.canceill@ens-cachan.org>
2013-10-26 23:04:16 +0200

    cosmetic and doc

--- pdfhide/pdf_algo.py
+++ pdfhide/pdf_algo.py
@@ -148,11 +148,21 @@ class PDF_stego:
 	def print_conf(self):
 		self.l.debug("===== CONFIG =====")
 		self.l.debugs({
-		  "input":"\"" + self.output + ".qdf\"",
-		  "redundancy":self.redundancy,
-		  "bit depth":self.nbits,
-		  "improvements":self.improve
-		  })
+			  "input":"\"" + self.output + ".qdf\"",
+			  "redundancy":self.redundancy,
+			  "bit depth":self.nbits,
+			  "improvements":self.improve
+			  })
+
+	def print_conf_embed(self,data,nums):
+		self.print_conf()
+		self.l.debugs({
+			  "Data to embed":data,
+			  "Data to embed (binary)":encoding.str_to_binstr(data,self.nbits),
+			  "FlagStr1 (CheckStr)":nums[0],
+			  "FlagStr2":nums[2],
+			  "Data":encoding.msg_to_nums(data,self.nbits)
+			  })
 
 	#
 	#
@@ -172,38 +182,67 @@ class PDF_stego:
 	# If res[0] == False then try to embed num again in the nex operator
 	# res[1] is the new operator value (regardless of res[0])
 	def embed_op(self,val,ch_one,ch_two,num):
+		# Check the original TJ value
 		if (not self.improve and abs(val) > 2**self.nbits) or val == 0:
-			# Do not use TJ op
+			# Value is invalid
+			# -> Do not use TJ op
 			return [False,val]
+		# Original TJ value is valid
+		# -> Use TJ op
 		self.tj_count += 1
+		# -> Check improvement flag
 		if self.improve:
+			# Improvements are enabled
+			# -> Check redundancy and custom range settings
 			if ch_two < self.redundancy or num == None or (self.customrange and not encoding.is_in_crange(val,self.nbits)):
-				# Custom range values work because of the hack normalrange/customrange
+				# NB: Custom range values work because of the hack normalrange/customrange
 				# TODO: update docs!!!
-				# Use TJ op for a random value
+				#
+				# Value is either out of range
+				# or ruled out by redundancy
+				# or all data is already embedded
+				# -> Check no-random flag
 				if self.norandom or self.customrange:
+					# No-random is set
+					# -> Do not use TJ op
 					return [False,val]
+				# Use TJ op for a random value
+				# -> Check sign
 				if val < 0:
 					return [False,-abs(val) + (abs(val) % (2**self.nbits)) - (int((2**self.nbits - 1) * ch_one) + 1 )]
 				return [False,abs(val) - (abs(val) % (2**self.nbits)) + int((2**self.nbits - 1) * ch_one) + 1]
-			# Use TJ op for data
+			# Value is in range
+			# -> Use TJ op for data
 			self.tj_count_valid += 1
-			normalrange = 1 # Hack for custom range (do not shift by 1), TODO: do that better and include in docs
+			normalrange = 1
+			# NB: Hack for custom range (do not shift by 1)
+			# TODO: do that better and include in docs
 			if self.customrange:
 				normalrange = 0
+			# Compute new TJ value
+			# -> Check sign
 			if val < 0:
-				#self.l.debug("Embed num ["+str(num)+"] as [ "+str(-abs(val) + (abs(val) % (2**self.nbits)) - num - 1)+" ("+str(val)+") ]")
 				return [True,-abs(val) + (abs(val) % (2**self.nbits)) - num - normalrange]
 			return [True,abs(val) - (abs(val) % (2**self.nbits)) + num + normalrange]
+		# Improvements are disabled
+		# -> Check redundancy setting
 		if ch_two < self.redundancy or num == None:
-			# Use TJ op for a random value
+			# Value is either ruled out by redundancy
+			# or all data is already embedded
+			# -> Check no-random flag
 			if self.norandom:
+				# No-random is set
+				# -> Do not use TJ op
 				return [False,val]
+			# Use TJ op for a random value
+			# -> Check sign
 			if val < 0:
 				return [False,-(int((2**self.nbits - 1) * ch_one) + 1 )]
 			return [False,int((2**self.nbits - 1) * ch_one) + 1]
-		# Use TJ op for data
+		# Value is in range
+		# -> Use TJ op for data
 		self.tj_count_valid += 1
+		# -> Check sign
 		if val < 0:
 			return [True, -num - 1]
 		return [True, num + 1]
@@ -213,44 +252,72 @@ class PDF_stego:
 	# line: the TJ string to parse
 	# ch_one: chaotic map 1
 	# ch_two: chaotic map 2
-	# ind: the IND to embed
-	# i: the current IND index
+	# ind: the list of nums to embed
+	# i: the number of TJ ops already used
+	# start: the TJ op where data starts
+	# ntjs: the number of TJ ops available
+	# j: the number of TJ ops already discarded
 	#
 	# Returns a list res[]
 	# res[0] is the modified line
 	# res[1] is the new value of the IND index
 	def embed_line(self,line,ch_one,ch_two,ind,i,start,ntjs,j):
+		# Copy parameters to return
 		newline = line
 		i_ = i
 		j_ = j
+		# Go through the line
 		k = 0
 		while k < newline.__len__():
-			# Parse TJ string from current position
+			# Look for a TJ op, starting at current position
 			m = re.search(r'[>)](\-?[0-9]+)[<(]',newline[k:])
 			if m == None:
 				# No more TJ ops
+				# -> Break the loop
 				k = newline.__len__()
 			else:
+				# A TJ op is found
 				tj = int(m.group(1))
+				# -> Check if there still is data to embed
 				if i_ < ind.__len__():
 					# Try to embed numeral
+					# -> Check improvements flag
 					if self.improve:
+						# Using Python's randomness
+						# -> Eliminate zeros
+						#
+						# TODO: check that
 						ch_one_next = 0
 						while ch_one_next == 0:
 							ch_one_next = ch_one.random()
 						ch_two_next = 0
 						while ch_two_next == 0:
 							ch_two_next = ch_two.random()
-						if self.tj_count < start: #TODO: fix
+						# Check the position of the TJ op in the file
+						# and embed accordingly
+						if self.tj_count < start: #TODO: fix -> TODO: remember what i meant by "fix"
+							# TJ op is before the start position
+							# -> Check the end position
 							if start + ind.__len__() + j_ - ntjs > self.tj_count:
+								# TJ op is before the end position
+								# -> Shift the list of nums accordingly
+								#    and embed num
 								op = self.embed_op(tj,ch_one_next,ch_two_next,ind[ntjs - start + self.tj_count - j_])
 							else:
+								# TJ op is after the end position
+								# -> Do not embed num
 								op = self.embed_op(tj,ch_one_next,ch_two_next,None)
+						# TJ op is after the start position
+						# -> Check if there is still data to embed
 						elif self.tj_count - start < ind.__len__() + j_:
+							# Embed num
 							op = self.embed_op(tj,ch_one_next,ch_two_next,ind[self.tj_count - start - j_])
 						else:
+							# Do not embed num
 							op = self.embed_op(tj,ch_one_next,ch_two_next,None)
 					else:
+						# Improvements are disabled
+						# -> Embed next num
 						op = self.embed_op(tj,ch_one.next(),ch_two.next(),ind[i_])
 				else:
 					# No more numerals to embed
@@ -259,102 +326,102 @@ class PDF_stego:
 					else:
 						op = self.embed_op(tj,ch_one.next(),ch_two.next(),None)
 				if op[0]:
-					# One numeral was embedded, update IND index
+					# One numeral was embedded, update valid index
 					i_ += 1
 				else:
+					# Numeral was not embedded, update discarded index
 					j_ += 1
-				# Insert new value
+				# Finished analizing TJ op
+				# -> Insert new value
 				newline = newline[:k + m.start(1)] + str(op[1]) + newline[k + m.end(1):]
 				# Update current position
+				# -> Jump after the current TJ op
 				k += m.start(1) + str(op[1]).__len__()
+				# -> Keep parsing the line
 		return [newline,i_,j_]
 
 	# Embeds data with passkey in a PDF file, outputs stego PDF file
 	#
 	# Returns the number of embedded numerals constituting the data
 	def embed(self,data,passkey,norandom=False):
-		self.print_conf()
+		# Initialize state
+		self.norandom = norandom
 		self.tj_count = 0
 		self.tj_count_valid = 0
-		self.norandom = norandom
-		self.l.info("Embedding data, please wait...")
-		driver.uncompress(self.input,self.input+".qdf")
-		cover_file = open(self.input + ".qdf","rb")
+		self.tjs = []
+		i = 0
+		j = 0
 		new_file = b""
 		# Get the numerals to embed from the key and the message
-		self.l.debug("Data to embed",data)
-		self.l.debug("Data to embed (binary)",encoding.str_to_binstr(data,self.nbits))
 		nums = encoding.encode_msg(data,passkey,self.nbits)
 		ind = nums[0] + nums[1] + nums[2]
-		self.tjs = []
-		self.l.debug("FlagStr1 (CheckStr)",nums[0])
-		self.l.debug("FlagStr2",nums[2])
-		self.l.debug("Data",encoding.msg_to_nums(data,self.nbits))
-		# Initiate chaotic maps
+		# Initialize chaotic maps
 		if self.improve:
 			ch_one = random.Random(encoding.digest(data))
 			ch_two = random.Random(passkey)
 		else:
 			ch_one = chaos.Chaotic(self.mu_one,nums[2])
 			ch_two = chaos.Chaotic(self.mu_two,nums[2])
-		# Parse file
+		# Determine start position
 		if 0:#self.improve: #TODO: fix
 			start = int(self.tjs.__len__() * ch_two.random())
 			self.l.debug("Random start position",start)
 		else:
 			start = 0
-		i = 0
-		j = 0
+		# Open input file
+		driver.uncompress(self.input,self.input+".qdf")
+		cover_file = open(self.input + ".qdf","rb")
 		cover_file.seek(0,0)
+		# Parse file
+		self.l.info("Embedding data, please wait...")
+		self.print_conf_embed(data,nums)
 		for line__ in cover_file:
 			line = line__.decode("latin-1")
 			line_ = line
+			# Parse line for TJ blocks
 			k = 0
 			while k < line_.__len__():
-				# Parse line for TJ blocks
+				# Look for a TJ block, starting at current position
 				m = re.match(r'\[(.*?)\][ ]?TJ',line_[k:])
 				if m == None:
 					# No TJ blocks
+					# -> Look further
 					k += 1
 				else:
-					# Try to embed data in TJ block
+					# A TJ block is found
+					# -> Try to embed data in TJ block
 					block = self.embed_line(m.group(1),ch_one,ch_two,ind,i,start,self.tjs.__len__(),j)
-					# Insert new block
+					# Insert new block in the line
 					line_ = line_[:k + m.start(1)] + block[0] + line_[k + m.end(1):]
+					# Update state
 					i = block[1]
 					j = block[2]
 					# Update current position
 					k += m.start(1) + block[0].__len__()
+			# Encode new line
 			new_file += line_.encode("latin-1")
-		self.tjss_ = []
-		#
-		# BEGIN DEBUG CHECKS
-		#
-		if self.l.DEBUG:#TODO: do that better
-			self.debug_embed_check_tj(cover_file)
-		#
-		# END DEBUG CHECKS
-		#
+		self.debug_embed_check_tj(cover_file)
+		# Close file and clean up
 		cover_file.close()
 		driver.delete(self.input+".qdf")
+		# Check if all data was embedded
 		if i < ind.__len__():
+			# All data was not embedded
+			# -> Fail
 			self.l.error("Not enough space available (only " + str(self.tj_count_valid) + " available, " + str(ind.__len__()) + " needed)")
-			return 0
+			return -ind.__len__()
 		else:
+			# All data was embedded
 			self.l.info("Done embedding.")
+			# -> Produce output file
 			output_file = open(self.output+".raw","wb")
 			output_file.write(new_file)
 			output_file.close()
+			# Fix Compress Clean
 			driver.fcc(self.output+".raw",self.output)
-			#
-			# BEGIN DEBUG CHECKS
-			#
-			if self.l.DEBUG:#TODO: do that better
-				self.debug_embed_print_sum()
-			#
-			# END DEBUG CHECKS
-			#
+			self.debug_embed_print_sum()
 			driver.delete(self.output+".raw.fix")
+			# All finished
 			self.l.info("Output file: \"" + self.output + "\"")
 			return nums[1].__len__()
 
@@ -519,51 +586,54 @@ class PDF_stego:
 	#
 
 	def debug_embed_check_tj(self,cover_file):
-		cover_file.seek(0,0)
-		tjss = []
-		# Parse file
-		for line__ in cover_file:
-			line = line__.decode("latin-1")
-			# Parse line for TJ blocks
-			m = re.search(r'\[(.*)\][ ]?TJ',line)
-			if m != None:
-				self.tjs += self.get_tjs(m.group(1))
-				tjss += self.get_tjs_signed(m.group(1))
-		self.tjss_ = tjss
-		#if self.improve:
-		#	self.l.debug(self.print_it("TJ values before",tjss))
-		#	self.l.debug(self.print_it("Low-bits TJ values before",map(lambda x: abs(x) % (2**self.nbits),tjss)))
-		#	self.l.debug(self.print_it("TJ average before",n.avg(tjss)))
-		#	self.l.debug(self.print_it("TJ unsigned average before",n.avg(tjs)))
+		if self.l.DEBUG:#TODO: do that better
+			self.tjss_ = []
+			cover_file.seek(0,0)
+			tjss = []
+			# Parse file
+			for line__ in cover_file:
+				line = line__.decode("latin-1")
+				# Parse line for TJ blocks
+				m = re.search(r'\[(.*)\][ ]?TJ',line)
+				if m != None:
+					self.tjs += self.get_tjs(m.group(1))
+					tjss += self.get_tjs_signed(m.group(1))
+			self.tjss_ = tjss
+			#if self.improve:
+			#	self.l.debug(self.print_it("TJ values before",tjss))
+			#	self.l.debug(self.print_it("Low-bits TJ values before",map(lambda x: abs(x) % (2**self.nbits),tjss)))
+			#	self.l.debug(self.print_it("TJ average before",n.avg(tjss)))
+			#	self.l.debug(self.print_it("TJ unsigned average before",n.avg(tjs)))
 
 	def debug_embed_print_sum(self):
-		embd_file = open(self.output+".raw.fix",encoding="iso-8859-1")
-		tjss = []
-		# Parse file
-		for line in embd_file:
-			# Parse line for TJ blocks
-			m = re.search(r'\[(.*)\][ ]?TJ',line)
-			if m != None:
-				self.tjs += self.get_tjs(m.group(1))
-				tjss += self.get_tjs_signed(m.group(1))
-		embd_file.close()
-		#if 0:#self.improve:
-		#	self.print_debug("TJ values after",tjss)
-		#	self.print_debug("Low-bits TJ values after",map(lambda x: abs(x) % (2**self.nbits),tjss))
-		#	self.print_debug("TJ average after",n.avg(tjss))
-		#	self.print_debug("TJ unsigned average after",n.avg(tjs))
-		i = 0
-		sbugs = []
-		while i < tjss.__len__():
-			if tjss[i] * self.tjss_[i] < 0:
-				sbugs += ["@[" + str(i) + "] orig. " + str(self.tjss_[i]) + " | new " + str(tjss[i])]
-			i += 1
-		if sbugs.__len__() > 0:
-			self.l.debug("Sign bugs",sbugs)
-			self.l.debug("Embedded data","\"" + data + "\"")
-			self.l.debug("Total nb of TJ ops",self.tj_count)
-			self.l.debug("Total nb of TJ ops used",ind.__len__())
-			self.l.debug("Total nb of TJ ops used for data",nums[1].__len__())
+		if self.l.DEBUG:#TODO: do that better
+			embd_file = open(self.output+".raw.fix",encoding="iso-8859-1")
+			tjss = []
+			# Parse file
+			for line in embd_file:
+				# Parse line for TJ blocks
+				m = re.search(r'\[(.*)\][ ]?TJ',line)
+				if m != None:
+					self.tjs += self.get_tjs(m.group(1))
+					tjss += self.get_tjs_signed(m.group(1))
+			embd_file.close()
+			#if 0:#self.improve:
+			#	self.print_debug("TJ values after",tjss)
+			#	self.print_debug("Low-bits TJ values after",map(lambda x: abs(x) % (2**self.nbits),tjss))
+			#	self.print_debug("TJ average after",n.avg(tjss))
+			#	self.print_debug("TJ unsigned average after",n.avg(tjs))
+			i = 0
+			sbugs = []
+			while i < tjss.__len__():
+				if tjss[i] * self.tjss_[i] < 0:
+					sbugs += ["@[" + str(i) + "] orig. " + str(self.tjss_[i]) + " | new " + str(tjss[i])]
+				i += 1
+			if sbugs.__len__() > 0:
+				self.l.debug("Sign bugs",sbugs)
+				self.l.debug("Embedded data","\"" + data + "\"")
+				self.l.debug("Total nb of TJ ops",self.tj_count)
+				self.l.debug("Total nb of TJ ops used",ind.__len__())
+				self.l.debug("Total nb of TJ ops used for data",nums[1].__len__())
 
 	def debug_extract_print_sum(self,checksum,checkstr,embedded):
 		self.l.debug("Data Checksum",checksum)
